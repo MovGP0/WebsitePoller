47515405d3b5f6ae4a9c91b41882344f77ea9712
Johann Dirry <movgp0@gmail.com>
2017-06-19 19:37:41 +0200

    fixing error handling for logging setup

--- WebsitePoller/HostConfiguratorLoggingExtensions.cs
+++ WebsitePoller/HostConfiguratorLoggingExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using Serilog;
 using Topshelf;
 using Topshelf.HostConfigurators;
 
@@ -6,6 +8,8 @@ namespace WebsitePoller
 {
     public static class HostConfiguratorLoggingExtensions
     {
+        private static ILogger Log => Serilog.Log.ForContext(typeof(HostConfiguratorLoggingExtensions));
+
         public static HostConfigurator ConfigureLogging(this HostConfigurator config, string serviceName, string machineName, string logName)
         {
             config.DependsOnEventLog();
@@ -18,25 +22,38 @@ namespace WebsitePoller
         private static void DeleteEventLogSource(string serviceName, string machineName)
         {
             if (!EventLog.SourceExists(serviceName)) return;
+            try
+            {
+                var permission = new EventLogPermission(EventLogPermissionAccess.Administer, machineName);
+                permission.Assert();
 
-            var permission = new EventLogPermission(EventLogPermissionAccess.Administer, machineName);
-            permission.Assert();
-
-            EventLog.DeleteEventSource(serviceName, machineName);
+                EventLog.DeleteEventSource(serviceName, machineName);
+            }
+            catch(Exception e)
+            {
+                Log.Error(e, e.Message);
+            }
         }
 
         private static void CreateEventLogSource(string serviceName, string machineName, string logName)
         {
             if (EventLog.SourceExists(serviceName)) return;
 
-            var permission = new EventLogPermission(EventLogPermissionAccess.Administer, machineName);
-            permission.Assert();
-
-            var eventSourceCreationData = new EventSourceCreationData(serviceName, logName)
+            try
             {
-                MachineName = machineName
-            };
-            EventLog.CreateEventSource(eventSourceCreationData);
+                var permission = new EventLogPermission(EventLogPermissionAccess.Administer, machineName);
+                permission.Assert();
+
+                var eventSourceCreationData = new EventSourceCreationData(serviceName, logName)
+                {
+                    MachineName = machineName
+                };
+                EventLog.CreateEventSource(eventSourceCreationData);
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, e.Message);
+            }
         }
     }
 }
