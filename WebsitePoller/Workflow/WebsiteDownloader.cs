028dafb0f0463202085a11c3b8043798ea2eeb6b
Johann Dirry <movgp0@gmail.com>
2017-06-19 20:45:20 +0200

    adding exception logging

--- WebsitePoller/Workflow/WebsiteDownloader.cs
+++ WebsitePoller/Workflow/WebsiteDownloader.cs
@@ -3,11 +3,14 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
+using Serilog;
 
 namespace WebsitePoller.Workflow
 {
     public sealed class WebsiteDownloader : IWebsiteDownloader
     {
+        private static ILogger Log => Serilog.Log.ForContext<WebsiteDownloader>();
+
         public WebsiteDownloader([NotNull]IPolicyFactory policyFactory)
         {
             PolicyFactory = policyFactory;
@@ -19,7 +22,14 @@ namespace WebsitePoller.Workflow
         public async Task TryDownloadWebsiteWithPolicyAsync(Uri url, string targetPath, CancellationToken cancellationToken)
         {
             var policy = PolicyFactory.CreateDownloadWebsitePolicy();
-            await policy.ExecuteAsync(token => DownloadWebsiteAsync(url, targetPath, token), cancellationToken);
+            try
+            {
+                await policy.ExecuteAsync(token => DownloadWebsiteAsync(url, targetPath, token), cancellationToken);
+            }
+            catch(Exception e)
+            {
+                Log.Error(e, e.Message);
+            }
         }
         
         private static async Task DownloadWebsiteAsync(Uri url, string targetPath, CancellationToken cancellationToken)
