03f03ebab62f7fe6ed5dae45c71c05c3e1d642e4
Johann Dirry <movgp0@gmail.com>
2017-06-24 19:40:58 +0200

    adding null checks on ToastNotifierWrapper

--- WebsitePoller/Workflow/ToastNotifierWrapper.cs
+++ WebsitePoller/Workflow/ToastNotifierWrapper.cs
@@ -1,18 +1,22 @@
+using System;
 using Windows.UI.Notifications;
+using JetBrains.Annotations;
 
 namespace WebsitePoller.Workflow
 {
     public sealed class ToastNotifierWrapper : IToastNotifier
     {
-        public ToastNotifierWrapper(ToastNotifier toastNotifier)
+        public ToastNotifierWrapper([NotNull]ToastNotifier toastNotifier)
         {
-            ToastNotifier = toastNotifier;
+            ToastNotifier = toastNotifier ?? throw new ArgumentNullException(nameof(toastNotifier));
         }
 
+        [NotNull]
         public ToastNotifier ToastNotifier { get; }
 
-        public void Show(ToastNotification notification)
+        public void Show([NotNull]ToastNotification notification)
         {
+            if(notification == null) throw new ArgumentNullException(nameof(notification));
             ToastNotifier.Show(notification);
         }
     }
