955fb2743aef9efd68169892520f531a7f2bfc48
Johann Dirry <movgp0@gmail.com>
2017-06-24 21:17:02 +0200

    adding null checks for WebsiteDownloader

--- WebsitePoller/Workflow/IWebsiteDownloader.cs
+++ WebsitePoller/Workflow/IWebsiteDownloader.cs
@@ -2,13 +2,14 @@
 using System.Threading;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
+using JetBrains.Annotations;
 
 namespace WebsitePoller.Workflow
 {
     public interface IWebsiteDownloader
     {
-        Task<HtmlDocument> GetWebsiteOrNullWithPolicyAndLoggingAsync(Uri url, string targetPath, CancellationToken cancellationToken);
-        Task<HtmlDocument> GetWebsiteOrNullWithPolicyAsync(Uri url, CancellationToken cancellationToken);
-        Task<HtmlDocument> GetWebsiteOrNullAsync(Uri url, CancellationToken cancellationToken);
+        Task<HtmlDocument> GetWebsiteOrNullWithPolicyAndLoggingAsync([NotNull]Uri url, [NotNull]string targetPath, CancellationToken cancellationToken);
+        Task<HtmlDocument> GetWebsiteOrNullWithPolicyAsync([NotNull]Uri url, CancellationToken cancellationToken);
+        Task<HtmlDocument> GetWebsiteOrNullAsync([NotNull]Uri url, CancellationToken cancellationToken);
     }
 }
--- WebsitePoller/Workflow/WebsiteDownloader.cs
+++ WebsitePoller/Workflow/WebsiteDownloader.cs
@@ -11,6 +11,7 @@ namespace WebsitePoller.Workflow
 {
     public sealed class WebsiteDownloader : IWebsiteDownloader
     {
+        [NotNull]
         private static ILogger Log => Serilog.Log.ForContext<WebsiteDownloader>();
 
         [NotNull]
@@ -23,12 +24,15 @@ namespace WebsitePoller.Workflow
             [NotNull]IPolicyFactory policyFactory, 
             [NotNull]Func<Uri, IRestClient> restClientFactory)
         {
-            PolicyFactory = policyFactory;
-            RestClientFactory = restClientFactory;
+            PolicyFactory = policyFactory ?? throw new ArgumentNullException(nameof(policyFactory));
+            RestClientFactory = restClientFactory ?? throw new ArgumentNullException(nameof(restClientFactory));
         }
 
         public async Task<HtmlDocument> GetWebsiteOrNullWithPolicyAndLoggingAsync(Uri url, string targetPath, CancellationToken cancellationToken)
         {
+            if(url == null) throw new ArgumentNullException(nameof(url));
+            if(targetPath == null) throw new ArgumentNullException(nameof(targetPath));
+
             try
             {
                 Log.Verbose($"Downloading {url}");
@@ -43,12 +47,16 @@ namespace WebsitePoller.Workflow
 
         public async Task<HtmlDocument> GetWebsiteOrNullWithPolicyAsync(Uri url, CancellationToken cancellationToken)
         {
+            if (url == null) throw new ArgumentNullException(nameof(url));
+            
             var policy = PolicyFactory.CreateDownloadWebsitePolicy();
             return await policy.ExecuteAsync(token => GetWebsiteOrNullAsync(url, token), cancellationToken);
         }
 
         public async Task<HtmlDocument> GetWebsiteOrNullAsync(Uri url, CancellationToken cancellationToken)
         {
+            if (url == null) throw new ArgumentNullException(nameof(url));
+
             var resource = url.PathAndQuery;
             var domain = url.GetDomain();
             var client = RestClientFactory(domain);
