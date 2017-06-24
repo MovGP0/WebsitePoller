ad2e372a3f18420706088a710e6eed58385989cb
Johann Dirry <movgp0@gmail.com>
2017-06-24 19:38:18 +0200

    testing HtmlDocumentFactory

---
+++ WebsitePoller.Tests/Workflow/HtmlDocumentFactoryTests.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WebsitePoller.Workflow;
+
+namespace WebsitePoller.Tests.Workflow
+{
+    [TestFixture]
+    public sealed class HtmlDocumentFactoryTests
+    {
+        public sealed class FromFileAsync
+        {
+            private static IAn An { get; }
+
+            [Test]
+            public void ShouldThrowArgumentNullExceptionWhenPathIsNull()
+            {
+                Assert.That(() => HtmlDocumentFactory.FromFileAsync(null), Throws.ArgumentNullException);
+            }
+
+            [Test]
+            public void ShouldThrowFileNotFoundExceptionWhenPathIsInvalid()
+            {
+                Assert.That(async() => await HtmlDocumentFactory.FromFileAsync("foobar.dat"), Throws.InstanceOf<FileNotFoundException>());
+            }
+
+            [Test]
+            public async Task ShouldReturnFileWhenFound()
+            {
+                var assemblyPath = An.AssemblyPath();
+                var path = Path.Combine(assemblyPath, "altbau-wohnungen.html");
+
+                var result = await HtmlDocumentFactory.FromFileAsync(path);
+
+                Assert.Multiple(() => { 
+                    Assert.That(result, Is.Not.Null, "was null");
+                    Assert.That(result.DocumentNode.InnerHtml.GetHashCode(), Is.EqualTo(1207631242), "hash code");
+                });
+            }
+        }
+    }
+}
--- WebsitePoller/Workflow/HtmlDocumentFactory.cs
+++ WebsitePoller/Workflow/HtmlDocumentFactory.cs
@@ -1,15 +1,21 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
+using JetBrains.Annotations;
 
 namespace WebsitePoller.Workflow
 {
     public static class HtmlDocumentFactory
     {
-        public static async Task<HtmlDocument> FromFileAsync(string cachedFilePath, CancellationToken cancellationToken = default(CancellationToken))
+        [NotNull]
+        public static async Task<HtmlDocument> FromFileAsync([NotNull]string cachedFilePath, CancellationToken cancellationToken = default(CancellationToken))
         {
+            if(cachedFilePath == null) throw new ArgumentNullException(nameof(cachedFilePath));
+            if (!File.Exists(cachedFilePath)) throw new FileNotFoundException("The file was not found.", cachedFilePath);
+
             cancellationToken.ThrowIfCancellationRequested();
 
             var cachedWebpage = new HtmlDocument();
